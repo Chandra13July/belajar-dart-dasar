2289a448280198d6a3cc820d515bd1d105aa2dd4
Chandra13July <code13july@gmail.com>
2025-03-11 20:23:06 +0700

    Kode Operator penugasan

---
+++ bin/operator_penugasan.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  var a = 10;
+
+  // a = a + 5;
+  a += 5;
+
+  print(a);
+
+  var i = 0;
+  // i++; // i = i + 1;
+
+  var j = i++; // j = i, i++
+  var k= ++i; // i++, k = (++i)
+
+  print(i);
+  print(j);
+  print(k);
+}
