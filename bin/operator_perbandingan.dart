e11bd94a6037a8746bb8249e27e12e788c444b19
Chandra13July <code13july@gmail.com>
2025-03-11 17:28:13 +0700

    Kode Operator Perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  print(10 == 10); 
+  print(10 != 10); 
+  print(10 > 10); 
+  print(10 < 10); 
+  print(10 >= 10); 
+  print(10 <= 10); 
+
+  // Operator perbandingan juga bisa digunakan untuk membandingkan dua buah string
+  print('Dart' == 'Dart');
+  print('Dart' != 'Dart'); 
+}
