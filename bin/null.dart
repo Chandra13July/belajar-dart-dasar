b8c8fc371a7d82065572b4427d685d47b9fafc51
Chandra13July <code13july@gmail.com>
2025-03-13 11:14:43 +0700

    Kode Null

---
+++ bin/null.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  int number1;
+  number1 = 10;
+  // number1 = null; // Error: A value of type 'Null' can't be assigned to a variable of type 'int'.
+
+  int? number2;
+  number2 = null;
+
+  print(number1); 
+  print(number2);
+}
