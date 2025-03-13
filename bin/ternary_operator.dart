6d503a629a5f0e3b76cca380a49c9fd2f9936e9c
Chandra13July <code13july@gmail.com>
2025-03-13 11:26:32 +0700

    Kode Ternary Operator

---
+++ bin/ternary_operator.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  var nilai = 75;
+  String ucapan = nilai >= 75 ? "Selamat, Anda Lulus!" : "Maaf, Anda Tidak Lulus!";
+
+  // if(nilai >= 75){
+  //   ucapan = "Selamat, Anda Lulus!";
+  // } else {
+  //   ucapan = "Maaf, Anda Tidak Lulus!";
+  // }y
+
+  print(ucapan);
+}
