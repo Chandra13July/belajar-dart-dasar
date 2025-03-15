3a0d5883989b8758b26bb7e74fa2214ad10db16c
Chandra13July <code13july@gmail.com>
2025-03-15 13:32:49 +0700

    Kode Scope

---
+++ bin/scope.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  var name = 'Chandra';
+
+  void sayHello(){
+
+    var hello = 'Hello $name';
+    print(hello);
+  }
+
+  sayHello();
+  // print(hello); error
+}
+
+void example(){
+
+  // sayHello(); error
+}
